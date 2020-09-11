6ef5500a6502caf101271704a1b01546d1091a8f
tsl <thomas.lindemeier@mojin-robotics.de>
2020-09-11 15:57:27 +0200

    adds path tracer

---
+++ painty/sbr/test/src/PathTracerTest.cxx
@@ -0,0 +1,30 @@
+/**
+ * @file PathTracerTest.cxx
+ * @author thomas lindemeier
+ *
+ * @brief
+ *
+ * @date 2020-09-11
+ *
+ */
+
+#include "gtest/gtest.h"
+#include "painty/sbr/PathTracer.hxx"
+
+TEST(PathTracerTest, Construct) {
+  painty::Mat3d tensors(1000, 1000);
+  for (auto& t : tensors) {
+    t = {1.0, 0.0, 0.0};
+  }
+
+  auto tracer = painty::PathTracer(tensors);
+
+  tracer.setMinLen(3);
+  tracer.setMaxLen(20);
+  tracer.setStep(5);
+  tracer.setFc(0.5);
+
+  const auto path = tracer.trace({500.0, 500.0});
+
+  EXPECT_EQ(path.size(), 3U);
+}
